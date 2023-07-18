2b8d7d4bab8665ab51f4b53a50f41a4c9b540416
Turtle <71213645+Turlt31@users.noreply.github.com>
2023-07-18 15:23:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -170,20 +170,21 @@ def playCW(word):
 def createGame():
     for i in root.winfo_children(): i.destroy()
     def createWord():
-        text = wordE.get().lower()
-        shift = 5
-        encrypted_text = ""
-        for char in text:
-            if char.isalpha():
-                if char.isupper():
-                    encrypted_char = chr((ord(char) - 65 + shift) % 26 + 65)
+        if len(wordE.get()) == 5:
+            text = wordE.get().lower()
+            shift = 5
+            encrypted_text = ""
+            for char in text:
+                if char.isalpha():
+                    if char.isupper():
+                        encrypted_char = chr((ord(char) - 65 + shift) % 26 + 65)
+                    else:
+                        encrypted_char = chr((ord(char) - 97 + shift) % 26 + 97)
                 else:
-                    encrypted_char = chr((ord(char) - 97 + shift) % 26 + 97)
-            else:
-                encrypted_char = char
-            encrypted_text += encrypted_char
-        outputE.delete(0, END)
-        outputE.insert(0, encrypted_text)
+                    encrypted_char = char
+                encrypted_text += encrypted_char
+            outputE.delete(0, END)
+            outputE.insert(0, encrypted_text)
 
 
 
